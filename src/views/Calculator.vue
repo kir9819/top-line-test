5e278375737e0a5f42143db764154c777a685c6d
Kir <kir9819@google.com>
2018-09-12 23:37:37 +0300

    init

---
+++ src/views/Calculator.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import Fraction from '@/components/Fraction.vue';
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    Fraction,
+  },
+};
+</script>
