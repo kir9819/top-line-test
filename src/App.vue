8795b6f4449d0792d60bc09c8fa84ba63a28e8ce
Kir <kir9819@google.com>
2018-09-14 23:52:16 +0300

    update routers style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/calculator">Calculator</router-link> |
-      <router-link to="/comments">Comments</router-link>
+      <router-link to="/calculator" class="link">Calculator</router-link>
+      <router-link to="/comments" class="link">Comments</router-link>
     </div>
     <router-view/>
   </div>
@@ -11,5 +11,20 @@
 <style>
 body {
   text-align: center;
+}
+.link {
+  text-decoration: none;
+  border: 1px solid lightgray;
+  border-radius: 2px;
+  color: black;
+  padding: 10px;
+  margin: 0 5px;
+}
+.link:hover {
+  background-color: lightgray;
+}
+#nav {
+  height: 39px;
+  margin-bottom: 10px;
 }
 </style>
